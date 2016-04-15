ed686ebf1327b95a7cb41c0531ae62475a8ebf2c
Michael Prysiazny <mike.prysiazny@sympatico.ca>
2016-04-14 23:09:07 -0400

    Implement readable objects
    Readable objects have messages associated with them that will display to
    the screen when clicked. The messages display on a component of GUIOutput.
    The message will go away when the user clicks a second time or when they
    look away from the object they are reading.

---
+++ Assets/Scripts/Readable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Pilgrim.Controller;
+
+public class Readable : Interactable
+{
+    [SerializeField] private string m_Message;
+    override public void Respond(PlayerManager m)
+    {
+        GuiOutput.Log("Read it");
+        m.ChangeContext(new ReadController(m, m_Message)); 
+    }
+}
