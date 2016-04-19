03fd09e9275dc2b5058e960a8085277985bb071c
Michael Marona <michaelkmarona@gmail.com>
2016-04-18 23:36:18 -0400

    Added Audio Trigger for Narration
    Added a simple audio trigger prefab, and an example audio clip. Insert the
    trigger prefab into the scene, and insert the audio clip into its
    AudioSource. Moving through should cause the audio clip to be played
    exactly once, then never again.

---
+++ Assets/Scripts/Narration/NarrationTriggerHandler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NarrationTriggerHandler : MonoBehaviour {
+
+    private bool m_played;
+
+	// Use this for initialization
+	void Start () {
+        m_played = false;
+	}
+
+    // Play the sound when you reach the trigger
+    // Then disable to never play it again
+    public void OnTriggerEnter(Collider col)
+    {
+        if (!m_played)
+        {
+            m_played = true;
+            GetComponent<AudioSource>().Play();
+        }
+        
+    }
+}
