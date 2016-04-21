840ec5a47aeab921256b08a7ec288ee5c36c1cbc
Michael Prysiazny <mike.prysiazny@sympatico.ca>
2016-04-20 21:29:34 -0400

    Move trigger methods to one folder

---
+++ Assets/Scripts/Triggers/JumpTrigger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using Pilgrim.Player;
+using Pilgrim.Controller;
+
+namespace Pilgrim.Trigger
+{
+    public class JumpTrigger : MonoBehaviour {
+        [SerializeField] private float m_LaunchAngle;
+        void OnTriggerEnter(Collider col)
+        {
+            PlayerManager pm = col.gameObject.GetComponent<PlayerManager>();
+            if (pm != null)
+            {
+                pm.ChangeContext(new JumpController(pm, m_LaunchAngle));
+            }
+
+        }
+
+        void OnTriggerExit(Collider col)
+        {
+            PlayerManager pm = col.gameObject.GetComponent<PlayerManager>();
+            if (pm != null)
+            {
+                pm.ChangeContext(new DefaultController(pm));
+            }
+        }
+    }
+}
