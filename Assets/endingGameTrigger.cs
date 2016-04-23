4119ea58fadc20c83cde93043c30e103d1b5eac6
Shubham <vshubh123@gmail.com>
2016-04-23 15:32:51 -0400

    ending fly animation

---
+++ Assets/endingGameTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class endingGameTrigger : MonoBehaviour {
+
+    public GameObject endingCamera;
+	// Use this for initialization
+
+
+    void OnTriggerEnter(Collider c)
+    {
+        AudioClip clip = Resources.Load<AudioClip>("ending");
+        Narrator.PlaySound(clip);
+        //endingCamera.GetComponent<Camera>().enabled = true;
+        gameEndCameraPan cameraPanScript = (gameEndCameraPan)endingCamera.GetComponent(typeof(gameEndCameraPan));
+        cameraPanScript.finishGame();
+
+    }
+}
