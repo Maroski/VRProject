49d4f7af38a14e28ff7f78e5a35e37c4018a6c46
Michael Prysiazny <mike.prysiazny@sympatico.ca>
2016-04-23 09:51:28 -0400

    Cleanup Assets directory
    The main scenes were scattered across the code base. Move all main scenes
    into a single directory which we should now use when editing the scenes.

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -216,7 +216,7 @@ namespace Pilgrim.Player
 
         public void Move(Vector3 displacement)
         {
-            if (!m_IsJumping && HasSkill(EAbility.Walk))
+            if (!m_IsJumping || HasSkill(EAbility.Walk))
             {
                 m_DesiredDisplacement += displacement.normalized;
             }
